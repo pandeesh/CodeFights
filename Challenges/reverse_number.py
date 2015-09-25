2c84f7c677a05632168fb76dd9535d6f7e968c80
Pandeesh <pandeesh@gmail.com>
2015-09-25 15:24:35 -0700

    Create reverse_number.py

---
+++ Challenges/reverse_number.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Find the reversed number from the given one.
+
+Example:
+For n = 6587 the answer is 7856.
+Note:
+For n = 20 the answer is 2.
+
+[input] integer n
+
+[output] integer
+
+"""
+__author__ = 'pandeesh'
+
+def reverse_number(n):
+    if str(n)[-1] == 0:
+        return int(str(n)[::-2])
+    return int(str(n)[::-1])
+
+#tests
+x = reverse_number(0)
+print(x)
