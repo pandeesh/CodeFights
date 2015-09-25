697427e71fcef02e3edf0d07517f715c7b314d85
Pandeesh <pandeesh@gmail.com>
2015-09-25 16:32:23 -0700

    Update sum_of_odd_nos.py

--- Challenges/sum_of_odd_nos.py
+++ Challenges/sum_of_odd_nos.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+"""
+Example
+For a = 3 and b = 5 the output should be 0.
+For a = 3006 and b = 4090 the output should be 1923016.
+
+Note
+If you don't want to read the text below, maybe your code won't pass time and memory limits. ;)
+
+[input] integer a
+
+The first integer, -1 < a < 1e8.
+[input] integer b
+
+The second integer, 0 < b < 1e8 + 1.
+[output] integer
+
+Return the answer modulo 10000007
+"""
+
 def sumofoddnumbers(a, b):
     j = 0
     for i in range(a,b)[1:]:
