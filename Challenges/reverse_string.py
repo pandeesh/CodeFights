455dae9cf2784c6bdb871ce181e779a789c6393d
Pandeesh <pandeesh@gmail.com>
2015-10-01 21:52:46 -0700

    Update reverse_string.py
    Added an alternate approach without using re module.

--- Challenges/reverse_string.py
+++ Challenges/reverse_string.py
@@ -17,9 +17,15 @@ __author__ = 'pandeesh'
 import re
 
 def reverse_string(str1):
+  """ using re module """
   return re.sub('[\W\d]', '', str1[::-1])
+
+def reverse_string_alternate(str1):
+  """ without using re module"""
+  return "".join([x for x in str1[::-1] if x.isalpha()])
 
 
 #tests
 print(reverse_string("pand1eesh123@"))
 print(reverse_string("pandeesh"))
+print(reverse_string_alternate("pandees1h2"))
