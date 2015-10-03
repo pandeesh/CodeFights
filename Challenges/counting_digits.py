2aa767f5c8431602dfd71ca4e597ba902449bcec
Pandeesh <pandeesh@gmail.com>
2015-10-03 15:57:55 -0700

    Create counting_digits.py

---
+++ Challenges/counting_digits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pyhton
+"""
+If you write numbers from 1 to N next to each other, how many digits will be written?
+
+Given number N, return the total number of digits written.
+
+Examples
+
+For N = 10 the answer should be 11, because [12345678910] has 11 digits.
+
+[input] integer N
+
+1 ≤ N ≤ 108.
+[output] integer
+
+The total number of digits.
+"""
+
+__author__ = 'pandeesh'
+
+def CountingDigits(N):
+  cnt = 0
+  for i in range(1,N+1):
+    cnt += len(str(i))
+  return cnt
+
+#tests
+print(CountingDigits(22))
+
