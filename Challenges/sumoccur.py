e95b017ce663a18d22503a428f9f98e19627e194
Pandeesh <pandeesh@gmail.com>
2015-10-05 01:54:17 -0700

    Update sumoccur.py

--- Challenges/sumoccur.py
+++ Challenges/sumoccur.py
@@ -12,9 +12,17 @@ For A = [ 2 , 2 , 2 , 4 , 4 , 5] the output should be 2 * 3 + 4 * 2 + 5 * 1 = 19
 1 ≤ |A| ≤ 105, 1 ≤ A[i] ≤ 9.
 [output] integer
 """
+
+"""
+It's pretty straightforward, no need of counter
+"""
+def sumOccur(A):
+  return sum(A)
+  
+#below solutions would work, but not necessary for this case
 from collections import Counter
 
-def sumOccur(A):
+def sumOccur1(A):
   counter = Counter(A)
   sum = 0
   for e, n in counter.most_common():
@@ -23,12 +31,13 @@ def sumOccur(A):
   
   
 #Alternative
-def sumOccur1(A):
+def sumOccur2(A):
    return sum([ e * n for e,n in Counter(A).most_common()])
 
 
 #tests
 print(sumOccur([1, 1, 1, 2, 2, 3]))
 print(sumOccur1([2, 3, 4, 2, 2, 3]))
+print(sumOccur2([2, 3, 4, 2, 2, 3]))
 
 
