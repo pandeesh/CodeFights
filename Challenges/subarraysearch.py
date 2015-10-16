b361a4497264cc0cff8cd395ad45486519793816
Pandeesh <pandeesh@gmail.com>
2015-10-15 22:31:29 -0700

    Update subarraysearch.py
    updated  the problem clearly

--- Challenges/subarraysearch.py
+++ Challenges/subarraysearch.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 """
+Determine if the given subarray is a subarray of the given array.
+A string is a subarray of another string if the first string is not longer than the second, 
+and all its letters are present in the second string in the exact same order, 
 but not necessarily consecutive. Cases of the letters do not matter.
 
 Example:
