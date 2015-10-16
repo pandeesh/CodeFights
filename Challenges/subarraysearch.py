a254ea52534bd54f025ea0be8c49c8252289e79f
Pandeesh <pandeesh@gmail.com>
2015-10-15 22:30:31 -0700

    Create subarraysearch.py

---
+++ Challenges/subarraysearch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+but not necessarily consecutive. Cases of the letters do not matter.
+
+Example:
+
+subarraysearch("ABCdefgh", "bCf) = true
+subarraysearch("ABCdefgh", 'i') = false
+
+[input] string array
+
+Input string, 2 < array < 10.
+[input] string subarray
+
+A subarray.
+[output] boolean
+
+true if subarray is a subarray of the array, false otherwise.
+"""
+
+def subarraysearch(a, s):
+  if len(a) <= len(s):
+    return False
+  else:
+    r = []
+    a = a.upper()
+    s = s.upper()
+    for i in range(len(s)):
+       if s[i] in list(a):
+          r.append(s[i])
+    if list(s) == r:
+       return True
+    return False
+   
+#tests
+print(subarraysearch("ABCdefgh", "bCf"))
+print(subarraysearch("ABCdefgh", 'i'))
